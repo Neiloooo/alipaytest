438c3594eb2372e4e50d76c948db20700ca811a7
Neio <1126819198@qq.com>
2019-06-05 18:29:18 +0800

    支付宝支付个人测试版

---
+++ src/main/java/com/test/alipay/App.java
@@ -0,0 +1,13 @@
+package com.test.alipay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
